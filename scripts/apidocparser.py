7a2ae794b3b150032c1e70e30c0b8313989e4a55
Nathan Osman <admin@quickmediasolutions.com>
2014-02-02 20:20:54 -0800

    Implemented lookup of parameters and their appropriate types.

--- scripts/apidocparser.py
+++ scripts/apidocparser.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from errno import EEXIST
 from hashlib import md5
-from json import dumps
+from json import dumps, loads
 from os import makedirs, path
+from re import compile
 from time import sleep, time
 from urllib2 import urlopen
 
@@ -13,7 +14,8 @@ class APIDocParser(object):
     Parses the Stack Exchange API documentation
     '''
     
-    _BASE_URL = 'http://api.stackexchange.com'
+    _BASE_URL     = 'http://api.stackexchange.com'
+    _PARAMETER_JS = compile(r'var\s+parameters\s*=\s*({.*});')
     
     def __init__(self, args):
         '''
@@ -40,28 +42,39 @@ class APIDocParser(object):
             if not e.errno == EEXIST:
                 raise
     
-    def _load_page(self, path):
+    def _load_page(self, page):
         '''
         Loads the specified page either directly or from the cache
         '''
-        key      = md5(path).hexdigest()
-        filename = path.join([self._args.cache_dir, key,])
-        if self._args.use_cache:
+        key      = md5(page).hexdigest()
+        filename = path.join(self._args.cache_dir, key)
+        if not self._args.bypass_cache:
             try:
                 with open(filename, 'r') as file:
                     return BeautifulSoup(file.read(), 'html5lib')
             except IOError:
                 pass
         sleep(max(1 - (time() - self._last_request), 0))
-        print 'Retrieving %s...' % path
-        content = urlopen(self._BASE_URL + path).read()
+        print 'Retrieving %s...' % page
+        content = urlopen(self._BASE_URL + page).read()
         self._last_request = time()
-        if self._args.use_cache:
+        if not self._args.bypass_cache:
             with open(filename, 'w') as file:
                 file.write(content)
         return BeautifulSoup(content, 'html5lib')
     
-    def _parse(self):
+    def _parse_method(self, method):
+        '''
+        Parses the specified method
+        '''
+        page   = self._load_page(method['href'])
+        script = page('script', text=self._PARAMETER_JS)
+        self._map['methods']['network'].append({
+            'name':       method.string,
+            'parameters': self._parse_parameters(script),
+        })
+    
+    def _parse_methods(self):
         '''
         Retrieves documentation pages and parses them
         '''
@@ -69,14 +82,12 @@ class APIDocParser(object):
         for div in root.find_all('div', 'method-name'):
             self._parse_method(div.a)
     
-    def _parse_method(self, method):
-        '''
-        Parses the specified method
-        '''
-        page = self._load_page(method['href'])
-        self._map['methods']['network'].append({
-            'name': method.string,
-        })
+    def _parse_parameters(self, script):
+        if not len(script):
+            return
+        match = self._PARAMETER_JS.search(script[0].string)
+        if match:
+            return loads(match.group(1))
     
     def _write_map(self):
         '''
@@ -90,16 +101,16 @@ class APIDocParser(object):
         '''
         Begins parsing the API docs
         '''
-        if self._args.use_cache:
+        if not self._args.bypass_cache:
             self._create_cache_dir()
-        self._parse()
+        self._parse_methods()
         self._write_map()
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Parses the documentation for the Stack Exchange API')
-    parser.add_argument('--use-cache',
+    parser.add_argument('--bypass-cache',
                         action='store_true',
-                        help='use a local cache to store content')
+                        help='do not use a local cache to store content')
     parser.add_argument('--cache-dir',
                         metavar='DIRECTORY',
                         type=str,
